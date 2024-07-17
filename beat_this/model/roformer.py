4a0a3acfdd8aed67a5d6ddc6ccff4d44f3db8e02
Jan Schlüter <jan.schlueter@jku.at>
2024-07-17 12:21:20 +0200

    Remove unused function

--- beat_this/model/roformer.py
+++ beat_this/model/roformer.py
@@ -156,19 +156,6 @@ class Transformer(Module):
         return x
 
 
-def once(fn):
-    called = False
-    @wraps(fn)
-    def inner(x):
-        nonlocal called
-        if called:
-            return
-        called = True
-        return fn(x)
-    return inner
-
-print_once = once(print)
-
 class Attend(nn.Module):
     def __init__(
         self,
