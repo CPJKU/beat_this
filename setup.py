24834b971a23e29236e7d95de29be2a055b89750
Jan Schlüter <jan.schlueter@jku.at>
2024-07-18 12:50:15 +0200

    Install prediction script along with package

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup, find_packages
 
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
 setup(
     name="beat_this",
     version="0.1",
-    author='Francesco Foscarin',
+    description="Beat This! beat tracker",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Francesco Foscarin, Jan Schlüter",
+    url="https://github.com/CPJKU/beat_this",
+    license="MIT",
     packages=find_packages(),
+    scripts=["bin/beat_this"],
     install_requires=[
-        "torch",
         "numpy",
-        "pandas",
-        "librosa",
+        "torch>=2",
+        "torchaudio",
+        "einops",
+        "rotary_embedding_torch",
+        "soxr",
     ],
+    python_requires='>=3',
 )
