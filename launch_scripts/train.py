e542e58543a76bba008e2b70657abdbf145f8c61
fosfrancesco <foscarin.francesco@gmail.com>
2024-07-19 16:35:14 +0200

    moved args outside main function for style coherency

--- launch_scripts/train.py
+++ launch_scripts/train.py
@@ -13,7 +13,125 @@ from beat_this.dataset.dataset import BeatDataModule
 from beat_this.model.pl_module import PLBeatThis
 
 
-def main():
+def main(args):
+    # for repeatability
+    seed_everything(args.seed, workers=True)
+
+    print("Starting a new run with the following parameters:")
+    print(args)
+
+    params_str = f"{'noval ' if not args.val else ''}{'hung ' if args.hung_data else ''}{'fold' + str(args.fold) + ' ' if args.fold is not None else ''}{args.loss}-h{args.transformer_dim}-aug{args.tempo_augmentation}{args.pitch_augmentation}{args.mask_augmentation}{' nosumH ' if not args.sum_head else ''}{' nopartialT ' if not args.partial_transformers else ''}"
+    if args.logger == "wandb":
+        logger = WandbLogger(
+            project="beat_this", name=f"{args.name} {params_str}".strip()
+        )
+    else:
+        logger = None
+
+    if args.force_flash_attention:
+        print("Forcing the use of the flash attention.")
+        torch.backends.cuda.enable_flash_sdp(True)
+        torch.backends.cuda.enable_mem_efficient_sdp(False)
+        torch.backends.cuda.enable_math_sdp(False)
+
+    data_dir = Path(__file__).parent.parent.relative_to(Path.cwd()) / "data"
+    checkpoint_dir = (
+        Path(__file__).parent.parent.relative_to(Path.cwd()) / "checkpoints"
+    )
+    augmentations = {}
+    if args.tempo_augmentation:
+        augmentations["tempo"] = {"min": -20, "max": 20, "stride": 4}
+    if args.pitch_augmentation:
+        augmentations["pitch"] = {"min": -5, "max": 6}
+    if args.mask_augmentation:
+        # kind, min_count, max_count, min_len, max_len, min_parts, max_parts
+        augmentations["mask"] = {
+            "kind": "permute",
+            "min_count": 1,
+            "max_count": 6,
+            "min_len": 0.1,
+            "max_len": 2,
+            "min_parts": 5,
+            "max_parts": 9,
+        }
+
+    datamodule = BeatDataModule(
+        data_dir,
+        batch_size=args.batch_size,
+        train_length=args.train_length,
+        spect_fps=args.fps,
+        num_workers=args.num_workers,
+        test_dataset="gtzan",
+        length_based_oversampling_factor=args.length_based_oversampling_factor,
+        augmentations=augmentations,
+        hung_data=args.hung_data,
+        no_val=not args.val,
+        fold=args.fold,
+    )
+    datamodule.setup()
+
+    # compute positive weights
+    pos_weights = datamodule.get_train_positive_weights(widen_target_mask=3)
+    print("Using positive weights: ", pos_weights)
+    dropout = {
+        "frontend": args.frontend_dropout,
+        "transformer": args.transformer_dropout,
+    }
+    pl_model = PLBeatThis(
+        spect_dim=128,
+        fps=50,
+        transformer_dim=args.transformer_dim,
+        ff_mult=4,
+        n_layers=args.n_layers,
+        stem_dim=32,
+        dropout=dropout,
+        lr=args.lr,
+        weight_decay=args.weight_decay,
+        pos_weights=pos_weights,
+        head_dim=32,
+        loss_type=args.loss,
+        warmup_steps=args.warmup_steps,
+        max_epochs=args.max_epochs,
+        use_dbn=args.dbn,
+        eval_trim_beats=args.eval_trim_beats,
+        sum_head=args.sum_head,
+        partial_transformers=args.partial_transformers,
+    )
+    for part in args.compile:
+        if hasattr(pl_model.model, part):
+            setattr(pl_model.model, part, torch.compile(getattr(pl_model.model, part)))
+            print("Will compile model", part)
+        else:
+            raise ValueError("The model is missing the part", part, "to compile")
+
+    callbacks = [LearningRateMonitor(logging_interval="step")]
+    # save only the last model
+    callbacks.append(
+        ModelCheckpoint(
+            every_n_epochs=1,
+            dirpath=str(checkpoint_dir),
+            filename=f"{args.name} S{args.seed} {params_str}".strip(),
+        )
+    )
+
+    trainer = Trainer(
+        max_epochs=args.max_epochs,
+        accelerator="auto",
+        devices=[args.gpu],
+        num_sanity_val_steps=1,
+        logger=logger,
+        callbacks=callbacks,
+        log_every_n_steps=1,
+        precision="16-mixed",
+        accumulate_grad_batches=args.accumulate_grad_batches,
+        check_val_every_n_epoch=args.val_frequency,
+    )
+
+    trainer.fit(pl_model, datamodule)
+    trainer.test(pl_model, datamodule)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--name", type=str, default="")
     parser.add_argument("--gpu", type=int, default=0)
@@ -159,120 +277,4 @@ def main():
 
     args = parser.parse_args()
 
-    # for repeatability
-    seed_everything(args.seed, workers=True)
-
-    print("Starting a new run with the following parameters:")
-    print(args)
-
-    params_str = f"{'noval ' if not args.val else ''}{'hung ' if args.hung_data else ''}{'fold' + str(args.fold) + ' ' if args.fold is not None else ''}{args.loss}-h{args.transformer_dim}-aug{args.tempo_augmentation}{args.pitch_augmentation}{args.mask_augmentation}{' nosumH ' if not args.sum_head else ''}{' nopartialT ' if not args.partial_transformers else ''}"
-    if args.logger == "wandb":
-        logger = WandbLogger(project="beat_this", name=f"{args.name} {params_str}".strip())
-    else:
-        logger = None
-
-    if args.force_flash_attention:
-        print("Forcing the use of the flash attention.")
-        torch.backends.cuda.enable_flash_sdp(True)
-        torch.backends.cuda.enable_mem_efficient_sdp(False)
-        torch.backends.cuda.enable_math_sdp(False)
-
-    data_dir = Path(__file__).parent.parent.relative_to(Path.cwd()) / "data"
-    checkpoint_dir = (
-        Path(__file__).parent.parent.relative_to(Path.cwd()) / "checkpoints"
-    )
-    augmentations = {}
-    if args.tempo_augmentation:
-        augmentations["tempo"] = {"min": -20, "max": 20, "stride": 4}
-    if args.pitch_augmentation:
-        augmentations["pitch"] = {"min": -5, "max": 6}
-    if args.mask_augmentation:
-        # kind, min_count, max_count, min_len, max_len, min_parts, max_parts
-        augmentations["mask"] = {
-            "kind": "permute",
-            "min_count": 1,
-            "max_count": 6,
-            "min_len": 0.1,
-            "max_len": 2,
-            "min_parts": 5,
-            "max_parts": 9,
-        }
-
-    datamodule = BeatDataModule(
-        data_dir,
-        batch_size=args.batch_size,
-        train_length=args.train_length,
-        spect_fps=args.fps,
-        num_workers=args.num_workers,
-        test_dataset="gtzan",
-        length_based_oversampling_factor=args.length_based_oversampling_factor,
-        augmentations=augmentations,
-        hung_data=args.hung_data,
-        no_val=not args.val,
-        fold=args.fold,
-    )
-    datamodule.setup()
-
-    # compute positive weights
-    pos_weights = datamodule.get_train_positive_weights(widen_target_mask=3)
-    print("Using positive weights: ", pos_weights)
-    dropout = {
-        "frontend": args.frontend_dropout,
-        "transformer": args.transformer_dropout,
-    }
-    pl_model = PLBeatThis(
-        spect_dim=128,
-        fps=50,
-        transformer_dim=args.transformer_dim,
-        ff_mult=4,
-        n_layers=args.n_layers,
-        stem_dim=32,
-        dropout=dropout,
-        lr=args.lr,
-        weight_decay=args.weight_decay,
-        pos_weights=pos_weights,
-        head_dim=32,
-        loss_type=args.loss,
-        warmup_steps=args.warmup_steps,
-        max_epochs=args.max_epochs,
-        use_dbn=args.dbn,
-        eval_trim_beats=args.eval_trim_beats,
-        sum_head=args.sum_head,
-        partial_transformers=args.partial_transformers,
-    )
-    for part in args.compile:
-        if hasattr(pl_model.model, part):
-            setattr(pl_model.model, part, torch.compile(getattr(pl_model.model, part)))
-            print("Will compile model", part)
-        else:
-            raise ValueError("The model is missing the part", part, "to compile")
-
-    callbacks = [LearningRateMonitor(logging_interval="step")]
-    # save only the last model
-    callbacks.append(
-        ModelCheckpoint(
-            every_n_epochs=1,
-            dirpath=str(checkpoint_dir),
-            filename=f"{args.name} S{args.seed} {params_str}".strip(),
-        )
-    )
-
-    trainer = Trainer(
-        max_epochs=args.max_epochs,
-        accelerator="auto",
-        devices=[args.gpu],
-        num_sanity_val_steps=1,
-        logger=logger,
-        callbacks=callbacks,
-        log_every_n_steps=1,
-        precision="16-mixed",
-        accumulate_grad_batches=args.accumulate_grad_batches,
-        check_val_every_n_epoch=args.val_frequency,
-    )
-
-    trainer.fit(pl_model, datamodule)
-    trainer.test(pl_model, datamodule)
-
-
-if __name__ == "__main__":
-    main()
+    main(args)
