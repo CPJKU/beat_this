2aa7234315e78e855dfc42003f5ef26cfe9fcf96
fosfrancesco <foscarin.francesco@gmail.com>
2024-07-26 10:42:58 +0200

    updated tests

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,21 +1,22 @@
-import pytest
+import soundfile as sf
 import numpy as np
 import torch
 from pathlib import Path
 
-from beat_this.inference import Audio2Beats, Audio2Frames
+from beat_this.inference import File2Beats, Audio2Frames
 
-def test_Audio2Beat():
-    a2b = Audio2Beats()
-    audio_path = Path("tests/Puttin On The Ritz - Kings of Swing.mp3")
-    beat, downbeat = a2b(audio_path)
+def test_File2Beat():
+    f2b = File2Beats()
+    audio_path = Path("tests/It Don't Mean A Thing - Kings of Swing.mp3")
+    beat, downbeat = f2b(audio_path)
     assert isinstance(beat, np.ndarray)
     assert isinstance(downbeat, np.ndarray)
     
-def test_Audio2Frame():
+def test_Audio2Frames():
     a2f = Audio2Frames()
-    audio_path = Path("tests/Puttin On The Ritz - Kings of Swing.mp3")
-    beat, downbeat = a2f(audio_path)
+    audio_path = Path("tests/It Don't Mean A Thing - Kings of Swing.mp3")
+    # load audio
+    audio, sr = sf.read(audio_path)
+    beat, downbeat = a2f(audio, sr)
     assert isinstance(beat, torch.Tensor)
     assert isinstance(downbeat, torch.Tensor)
-    
