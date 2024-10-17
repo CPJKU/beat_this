30cbb2af25c5a1c187ff36f71564d20ee6cbb1c7
Jan Schlüter <jan.schlueter@jku.at>
2024-10-17 16:19:57 +0200

    Replace file operations in MemoryMappedNpzFile to be multiprocessing-safe

--- beat_this/dataset/mmnpz.py
+++ beat_this/dataset/mmnpz.py
@@ -3,8 +3,6 @@ Support for memory-mapping uncompressed .npz files.
 """
 from collections.abc import Mapping
 from zipfile import ZipFile
-from threading import Lock
-from contextlib import nullcontext
 import struct
 
 import numpy as np
@@ -21,8 +19,6 @@ class MemmappedNpzFile(Mapping):
     files : list of str
         List of all uncompressed files in the archive with a ``.npy`` extension
         (listed without the extension). These are supported as dictionary keys.
-    zip : ZipFile instance
-        The ZipFile object initialized with the zipped archive.
     mmap : np.memmap
         The memory map of the full .npz file.
     arrays : dict
@@ -36,38 +32,33 @@ class MemmappedNpzFile(Mapping):
         Whether to cache array objects in case they are requested again.
     preload : bool, optional
         Whether to precreate all array objects upon opening. Enforces caching.
-    threadsafe : bool, optional
-        Whether to guard multi-threaded access with a lock when not preloading.
     """
-    def __init__(self, fn: str, cache: bool = True, preload: bool = False,
-                 threadsafe: bool = False):
-        self.zip = ZipFile(fn, mode='r')
+    def __init__(self, fn: str, cache: bool = True, preload: bool = False):
+        with ZipFile(fn, mode='r') as f:
+            self._offsets = {zinfo.filename[:-4]: (zinfo.header_offset,
+                                                   zinfo.file_size)
+                             for zinfo in f.infolist()
+                             if zinfo.filename.endswith('.npy')
+                             and zinfo.compress_type == 0}
+        self.files = list(self._offsets.keys())
         self.mmap = np.memmap(fn, mode='r')
         self.cache = cache or preload
         self.preload = preload
-        self.files = [zinfo.filename[:-4] for zinfo in self.zip.infolist()
-                      if zinfo.filename.endswith('.npy')
-                      and zinfo.compress_type == 0]
         if self.preload:
             self.arrays = {name: self.load(name) for name in self.files}
         else:
             self.arrays = {}
-        if threadsafe:
-            self.lock = Lock()
-        else:
-            self.lock = nullcontext()
 
-    def load(self, name):
-        zinfo = self.zip.getinfo(name + '.npy')
-        fp = self.zip.fp
+    def load(self, name: str):
+        header_offset, file_size = self._offsets[name]
         # parse lengths of local header file name and extra fields
-        # (zinfo is based on the global directory, not local header)
-        fp.seek(zinfo.header_offset + 26)
-        fn_len, extra_len = struct.unpack('<2H', fp.read(4))
+        # (ZipInfo is based on the global directory, not local header)
+        fn_len, extra_len = struct.unpack('<2H', self.mmap[header_offset + 26:header_offset + 30])
         # compute offset of start and end of data
-        npy_start = zinfo.header_offset + 30 + fn_len + extra_len
-        npy_end = npy_start + zinfo.file_size
+        npy_start = header_offset + 30 + fn_len + extra_len
+        npy_end = npy_start + file_size
         # read NPY header
+        fp = MemoryviewIO(self.mmap)
         fp.seek(npy_start)
         version = np.lib.format.read_magic(fp)
         np.lib.format._check_version(version)
@@ -77,10 +68,9 @@ class MemmappedNpzFile(Mapping):
         return self.mmap[data_start:npy_end].view(dtype=dtype).reshape(shape, order='F' if fortran else 'C')
 
     def close(self):
-        if hasattr(self, 'zip'):
-            self.zip.close()
         if hasattr(self, 'mmap'):
-            self.mmap.close()
+            del self.mmap
+        self.arrays = {}
 
     def __enter__(self):
         return self
@@ -94,18 +84,45 @@ class MemmappedNpzFile(Mapping):
     def __len__(self):
         return len(self.files)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str):
         if self.cache:
             try:
                 return self.arrays[key]
             except KeyError:
                 pass
-        with self.lock:
-            array = self.load(key)
+        array = self.load(key)
         if self.cache:
             self.arrays[key] = array
         return array
 
-    def __contains__(self, key):
+    def __contains__(self, key: str):
         # Mapping.__contains__ calls __getitem__, which could be expensive
-        return (key in self.files)
+        return (key in self._offsets)
+
+
+class MemoryviewIO(object):
+    """
+    Wraps an object supporting the buffer protocol to be a readonly file-like.
+    """
+    def __init__(self, buffer):
+        self._buffer = memoryview(buffer).cast('B')
+        self._pos = 0
+        self.seekable = lambda: True
+        self.readable = lambda: True
+        self.writable = lambda: False
+
+    def seek(self, offset, whence=0):
+        if whence == 0:
+            self._pos = offset
+        elif whence == 1:
+            self._pos += offset
+        elif whence == 2:
+            self._pos = self._buffer.nbytes + offset
+
+    def read(self, size=-1):
+        data = self._buffer[self._pos:self._pos + size if size >= 0 else None].tobytes()
+        self._pos += len(data)
+        return data
+
+    def tell(self):
+        return self._pos
